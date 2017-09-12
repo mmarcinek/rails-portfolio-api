5a5cb1458b20dd5de7a679228d72162a4bb5ac7a
Michael Marcinek <michaelmarcinek@gmail.com>
2017-09-12 15:19:18 -0400

    updates specs

---
+++ spec/factories/apps.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do 
+  factory :app do
+    name { Faker::TwinPeaks.character}
+  end
+end
---
+++ spec/factories/projects.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :project do
+    title { Faker::Lorem.word }
+    created_by{ Faker:Number.number(10) }
+  end
+end
