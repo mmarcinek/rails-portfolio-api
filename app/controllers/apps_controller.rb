bb3c652fb5199f9ccca0c13c6c7de0e0da7f6e33
Michael Marcinek <michaelmarcinek@gmail.com>
2017-09-13 11:23:43 -0400

    adds basic resource routes for apps

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -1,2 +1,36 @@
 class AppsController < ApplicationController
+  before_action :set_app, only: [:show, :update, :destroy]
+
+  # GET /projects/:project_id/apps
+  def index
+    @apps = App.where("project_id = ?", params['project_id']).all
+    json_response(@apps)
+  end
+
+  # POST /projects/:project_id/apps
+  def create
+    @app = App.create!(params)
+    json_response(@app, :created)
+  end
+
+  # GET /projects/:project_id/apps/:id
+  def show
+    json_response(@app)
+  end
+
+  # PUT /projects/:project_id/apps/:id
+  def update
+    @app.update(params)
+    head :no_content
+  end
+
+  # DELETE /projects/:project_id/apps/:id
+  def destroy
+    @app.destroy
+    head :no_content
+  end
+  
+  def set_app
+    @app = App.find("project_id =? AND id=?", params['project_id'], params['id'])
+  end
 end
