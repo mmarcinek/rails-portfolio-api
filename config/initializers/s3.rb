36c254bd63f9388497bdafbc4ca4b33c0dc374ed
Michael Marcinek <michaelmarcinek@gmail.com>
2017-09-15 14:31:51 -0400

    error on CarrierWave.config

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,5 +1,5 @@
 CarrierWave.configure do |config|
-  config.fog_credentials => {
+  config.fog_credentials = {
     :provider              => 'AWS'
     :aws_access_key_id     => ENV['S3_KEY']
     :aws_secret_access_key => ENV['S3_SECRET']
